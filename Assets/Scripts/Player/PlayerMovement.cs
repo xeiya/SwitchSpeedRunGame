b1ca80061387ee073bd1383f71ba623aa9d49448
xeiya <167661996+xeiya@users.noreply.github.com>
2025-05-05 21:37:36 +1000

    Started Game + added movement

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,265 @@
+using System.Collections;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Movement")]
+    private float moveSpeed;
+    public float runSpeed;
+    public float slideSpeed;
+    public float wallrunSpeed;
+
+    private float desiredMoveSpeed;
+    private float lastDesiredMoveSpeed;
+
+    public float speedIncreaseMultiplier;
+    public float slopeIncreaseMultiplier;
+
+    public float groundDrag;
+
+    [Header("Jumping")]
+    public float jumpForce;
+    public float jumpCooldown;
+    public float airMultiplier;
+    bool readyToJump = true;
+
+    [Header("Keybinds")]
+    public KeyCode jumpKey = KeyCode.Space;
+
+    [Header("Ground Check")]
+    public float playerHeight;
+    public LayerMask whatIsGround;
+    bool grounded;
+
+    [Header("Slope Handling")]
+    public float maxSlopeAngle;
+    private RaycastHit slopeHit;
+    private bool exitingSlope;
+
+    public Transform orientation;
+
+    float horizontalInput;
+    float verticalInput;
+
+    Vector3 moveDirection;
+
+    Rigidbody rb;
+
+    public MovementState state;
+    public enum MovementState 
+    { 
+        running,
+        wallrunning,
+        sliding,
+        air
+    }
+
+    public bool sliding;
+    public bool wallrunning;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    private void Update()
+    {
+        //ground check
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
+
+        MyInput();
+        SpeedControl();
+        StateHandler();
+
+        //handle drag
+        if (grounded)
+        {
+            rb.linearDamping = groundDrag;
+        }
+        else
+        {
+            rb.linearDamping = 0;
+        }
+    }
+
+    public void StateHandler() 
+    {
+        //Mode - Wallrunning
+        if (wallrunning) 
+        {
+            state = MovementState.wallrunning;
+            desiredMoveSpeed = wallrunSpeed;
+        }
+
+        // Mode - Sliding
+        if (sliding) 
+        {
+            state = MovementState.sliding;
+
+            if (OnSlope() && rb.linearVelocity.y < 0.1f) 
+            {
+                desiredMoveSpeed = slideSpeed;
+            }
+            else
+            {
+                desiredMoveSpeed = runSpeed;
+            }
+        }
+
+        else if (grounded) 
+        { 
+            state = MovementState.running;
+
+            desiredMoveSpeed = runSpeed;
+        }
+
+        else { state = MovementState.air; }
+
+        //check if desiredMoveSpeed has changed drastically
+        if (Mathf.Abs(desiredMoveSpeed - lastDesiredMoveSpeed) > 4f && moveSpeed != 0)
+        {
+            StopAllCoroutines();
+            StartCoroutine(SmoothlyLerpMoveSpeed());
+        }
+        else 
+        {
+            moveSpeed = desiredMoveSpeed;
+        }
+
+        lastDesiredMoveSpeed = desiredMoveSpeed;
+    }
+
+    private IEnumerator SmoothlyLerpMoveSpeed() 
+    {
+        //smoothly lerp movementSpeed to desired value
+        float time = 0;
+        float difference = Mathf.Abs(desiredMoveSpeed - moveSpeed);
+        float startValue = moveSpeed;
+
+        while (time < difference)
+        { 
+            moveSpeed = Mathf.Lerp(startValue, desiredMoveSpeed, time / difference);
+
+            if (OnSlope())
+            {
+                float slopeAngle = Vector3.Angle(Vector3.up, slopeHit.normal);
+                float slopeAngleIncrease = 1 + (slopeAngle / 90f);
+
+                time += Time.deltaTime * speedIncreaseMultiplier * slopeIncreaseMultiplier * slopeAngleIncrease;
+            }
+            else 
+            {
+                time += Time.deltaTime * speedIncreaseMultiplier;
+            }
+
+            yield return null;
+        }
+    }
+
+    private void MyInput() 
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+        //when to jump
+        if (Input.GetKey(jumpKey) && readyToJump && grounded) 
+        {
+            readyToJump = false;
+
+            Jump();
+
+            Invoke(nameof(ResetJump), jumpCooldown);
+        }
+    }
+
+    private void MovePlayer() 
+    { 
+        //calculate movement direction
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+        //on slope
+        if (OnSlope() && !exitingSlope) 
+        { 
+            rb.AddForce(GetSlopeMoveDirection(moveDirection) * moveSpeed * 20f, ForceMode.Force);
+            if (rb.linearVelocity.y > 0) 
+            {
+                rb.AddForce(Vector3.down * 80f, ForceMode.Force);
+            }
+        }
+
+        //on ground
+        if (grounded) { rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force); }
+
+        //in air
+        else if (!grounded) { rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force); }
+
+        //turn gravity off while on slope
+        if(!wallrunning) rb.useGravity = !OnSlope();
+    }
+
+    private void SpeedControl() 
+    {
+        //limiting speed on slope
+        if (OnSlope() && !exitingSlope)
+        {
+            if (rb.linearVelocity.magnitude > moveSpeed)
+            {
+                rb.linearVelocity = rb.linearVelocity.normalized * moveSpeed;
+            }
+        }
+
+        //limiting speed on ground or air
+        else 
+        {
+            Vector3 flatVel = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
+
+            //limit velocity if needed
+            if (flatVel.magnitude > moveSpeed)
+            {
+                Vector3 limitedVel = flatVel.normalized * moveSpeed;
+                rb.linearVelocity = new Vector3(limitedVel.x, rb.linearVelocity.y, limitedVel.z);
+            }
+        }
+
+            
+    }
+
+    private void Jump() 
+    {
+        exitingSlope = true;
+
+        //reset y velocity
+        rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
+
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+
+    private void ResetJump() 
+    {
+        readyToJump = true;
+
+        exitingSlope = false;
+    }
+
+    public bool OnSlope() 
+    {
+        if (Physics.Raycast(transform.position, Vector3.down, out slopeHit, playerHeight * 0.5f + 0.3f)) 
+        {
+            float angle = Vector3.Angle(Vector3.up, slopeHit.normal);
+            return angle < maxSlopeAngle && angle != 0f;
+        }
+
+        return false;
+    }
+
+    public Vector3 GetSlopeMoveDirection(Vector3 direction) 
+    {
+        return Vector3.ProjectOnPlane(direction, slopeHit.normal).normalized;
+    }
+}
---
+++ Assets/Scripts/Player/WallRunning.cs
@@ -0,0 +1,173 @@
+using UnityEngine;
+
+public class WallRunning : MonoBehaviour
+{
+    [Header("Wallrunning")]
+    public LayerMask whatIsWall;
+    public LayerMask whatIsGround;
+    public float wallRunForce;
+    public float wallJumpUpForce;
+    public float wallJumpSideForce;
+    public float maxWallRunTime;
+    private float wallRunTimer;
+
+    [Header("Input")]
+    public KeyCode jumpKey = KeyCode.Space;
+    private float horizontalInput;
+    private float verticalInput;
+
+    [Header("Detection")]
+    public float wallCheckDistance;
+    public float minJumpHeight;
+    private RaycastHit leftWallHit;
+    private RaycastHit rightWallHit;
+    private bool wallLeft;
+    private bool wallRight;
+
+    [Header("Exiting")]
+    private bool exitingWall;
+    public float exitWallTime;
+    private float exitWallTimer;
+
+    [Header("Gravity")]
+    public bool useGravity;
+    public float gravityCounterForce;
+
+    [Header("Reference")]
+    public Transform orientation;
+    public PlayerCam cam;
+    private PlayerMovement pm;
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        pm = GetComponent<PlayerMovement>();
+    }
+
+    private void Update()
+    {
+        CheckForWall();
+        StateMachine();
+    }
+
+    private void FixedUpdate()
+    {
+        if (pm.wallrunning)
+        {
+            WallRunningMovement();
+        }
+    }
+
+    private void CheckForWall() 
+    {
+        wallRight = Physics.Raycast(transform.position, orientation.right, out rightWallHit, wallCheckDistance, whatIsWall);
+        wallLeft = Physics.Raycast(transform.position, -orientation.right, out leftWallHit, wallCheckDistance, whatIsWall);
+    }
+
+    private bool AboveGround() 
+    {
+        return !Physics.Raycast(transform.position, Vector3.down, minJumpHeight, whatIsGround);
+    }
+
+    private void StateMachine() 
+    {
+        //Getting Inputs
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+        //State 1 - Wallrunning
+        if ((wallLeft || wallRight) && verticalInput > 0 && AboveGround() && !exitingWall)
+        {
+            if (!pm.wallrunning) { StartWallRun(); }
+
+            //wallrun time
+            if (wallRunTimer > 0) { wallRunTimer -= Time.deltaTime; }
+
+            if (wallRunTimer <= 0 && pm.wallrunning) 
+            { 
+                exitingWall = true;
+                exitWallTimer = exitWallTime;
+            }
+
+            //wall jump
+            if (Input.GetKeyDown(jumpKey)) { WallJump(); }
+        }
+
+        //State 2 - Exiting
+        else if (exitingWall) 
+        {
+            if (pm.wallrunning) { StopWallRun(); }
+
+            if (exitWallTimer > 0) { exitWallTimer -= Time.deltaTime; }
+
+            if (exitWallTimer < 0) { exitingWall = false; }
+        }
+
+        //State 3 - None
+        else
+        {
+            if (pm.wallrunning) { StopWallRun(); }
+        }
+    }
+
+    private void StartWallRun() 
+    { 
+        pm.wallrunning = true;
+
+        wallRunTimer = maxWallRunTime;
+
+        rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
+
+        //apply camera effects
+        cam.DoFov(85f);
+        if (wallLeft) { cam.DoTilt(-8f); }
+        if (wallRight) { cam.DoTilt(8f); }
+    }
+
+    private void WallRunningMovement() 
+    {
+        rb.useGravity = useGravity;
+
+        Vector3 wallNormal = wallRight ? rightWallHit.normal : leftWallHit.normal;
+
+        Vector3 wallForward = Vector3.Cross(wallNormal, transform.up);
+
+        if ((orientation.forward - wallForward).magnitude > (orientation.forward - -wallForward).magnitude) 
+        { wallForward = -wallForward; }
+
+        //forward force
+        rb.AddForce(wallForward * wallRunForce, ForceMode.Force);
+
+        //push to wall force
+        if (!(wallLeft && horizontalInput > 0) && (wallRight && horizontalInput < 0))
+        { rb.AddForce(-wallNormal * 100, ForceMode.Force); }
+
+        //weaken gravity
+        if (useGravity) { rb.AddForce(transform.up * gravityCounterForce, ForceMode.Force); }
+    }
+
+    private void StopWallRun() 
+    {
+        pm.wallrunning = false;
+
+        //reset camera effects
+        cam.DoFov(80f);
+        cam.DoTilt(0f);
+    }
+
+    private void WallJump() 
+    {
+        //enter exiting wall state
+        exitingWall = true;
+        exitWallTimer = exitWallTime;
+
+        Vector3 wallNormal = wallRight ? rightWallHit.normal : leftWallHit.normal;
+
+        Vector3 forceToApply = transform.up * wallJumpUpForce + wallNormal * wallJumpSideForce;
+
+        //add force
+        rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
+        rb.AddForce(forceToApply, ForceMode.Impulse);
+    }
+}
