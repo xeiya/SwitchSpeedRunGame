eaa3746803b046dc5a49b77ad2b4979e560125be
xeiya <167661996+xeiya@users.noreply.github.com>
2025-05-15 11:52:49 +1000

    Added a cooldown to the switch

--- Assets/Scripts/Player/SwitchAbility.cs
+++ Assets/Scripts/Player/SwitchAbility.cs
@@ -1,31 +1,68 @@
 using UnityEngine;
-using UnityEngine.UIElements;
+using UnityEngine.UI;
 
 public class SwitchAbility : MonoBehaviour
 {
+    [Header("Game Objects")]
     public GameObject overWorld;
     public GameObject underWorld;
 
-    private void Update()
+    [Header("Cooldown")]
+    public float switchCooldown;
+    private float lastTimeCasted;
+    private bool switchReady = true;
+
+    [Header("Cooldown Image")]
+    public Image switchCooldownBar;
+
+    private void Start()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse1)) 
+        switchReady = true;
+        switchCooldownBar.fillAmount = 1;
+    }
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse1) && switchReady == true) 
         {
-            ChangeWorld1();
-            ChangeWorld2();
+            ChangeWorld();
+            SwitchBarEmpty();
+        }
+
+        switchCooldownBar.fillAmount = Mathf.MoveTowards(switchCooldownBar.fillAmount, 1, Time.deltaTime / switchCooldown);
+        if (switchCooldownBar.fillAmount >= 1) 
+        { 
+            switchReady = true;
         }
     }
 
-    void ChangeWorld1() 
+    void ChangeWorld() 
     {
-        if (overWorld.activeInHierarchy == false)
-        { overWorld.SetActive(true); }
-        else { overWorld.SetActive(false); }
+        /*Checks for the last time casted by the.
+         *Time.time, if Time.time is more than last time casted
+         *Cast the ability else, return
+        */
+        if (Time.time < lastTimeCasted + switchCooldown && switchReady == false)
+            return;
+        lastTimeCasted = Time.time;
+
+        if (Time.time < lastTimeCasted + switchCooldown && switchReady == true) 
+        {
+            if (overWorld.activeInHierarchy == false)
+            { overWorld.SetActive(true); }
+            else if (overWorld.activeInHierarchy == true)
+            { overWorld.SetActive(false); }
+
+            if (underWorld.activeInHierarchy == false)
+            { underWorld.SetActive(true); }
+            else if (underWorld.activeInHierarchy == true)
+            { underWorld.SetActive(false); }
+
+            switchReady = false;
+        } 
     }
 
-    void ChangeWorld2()
+    public void SwitchBarEmpty() 
     {
-        if (underWorld.activeInHierarchy == false)
-        { underWorld.SetActive(true); }
-        else { underWorld.SetActive(false); }
+        switchCooldownBar.fillAmount = 0;
     }
 }
